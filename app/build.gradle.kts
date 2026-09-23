f79b0bf1560b6c9f8a0cba22eb02273e95bf6218
michaelhmiv <34044957+michaelhmiv@users.noreply.github.com>
2026-09-23 08:02:12 -0400

    Resolve Compose UI test dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     testImplementation("junit:junit:4.13.2")
 
+    androidTestImplementation(platform(composeBom))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     androidTestImplementation("androidx.test:core:1.7.0")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
